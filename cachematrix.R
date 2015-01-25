41e85801781770e5fedbdc81cbf1e1ae45d60fa5
Jonathan Taylor <jon@stackhaus.com>
2015-01-25 14:29:54 +0000

    completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,21 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(our_matrix = matrix()) {
+  computed_inverse <- NULL
+  
+  set <- function(new_matrix) {
+    our_matrix <<- new_matrix #Update our matrix to the new one
+    computer_inverse <<- NULL #Clear the cached value as the matrix has changed (so we recompute it)
+  }
+  
+  get <- function() our_matrix
+  
+  setInverse <- function(inverted_matrix) computed_inverse <<- inverted_matrix
+  
+  getInverse <- function() computed_inverse
+  
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
@@ -12,4 +25,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  
+  data <- x$get()
+  inverse <- solve(data) #Not using ... here we need to force defaults otherwise we can get different answers for the same input matrix!
+  x$setInverse(inverse)
+  inverse
 }
